86a7d8350927a99552690996ea9f8abe9a1c81b2
Manan Sharma <mansharm71@gmail.com>
2024-06-20 17:43:53 -0400

    sfafs

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,5 @@
-// src/components/SideBar.tsx
 import React, { useState, useEffect } from "react";
-import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/solid";
+import { ChevronRightIcon } from "@heroicons/react/solid";
 
 const GitLogo = require("../assets/logos/GitLogo.png");
 const LinkedinLogo = require("../assets/logos/LinkedinLogo.png");
@@ -10,22 +9,17 @@ const MailLogo = require("../assets/logos/MailLogo.png");
 const JSIcon = require("../assets/icons/JSIcon.png");
 const TSIcon = require("../assets/icons/TSIcon.png");
 
-interface SideBarProps {
-  setWidth: React.Dispatch<React.SetStateAction<number>>;
-  width: number;
-  activeTab: string; // Define activeTab prop
-  setActiveTab: React.Dispatch<React.SetStateAction<string>>; // Define setActiveTab prop
-}
-
-const SideBar: React.FC<SideBarProps> = ({
+const SideBar = ({
   setWidth,
   width,
   activeTab,
   setActiveTab,
+}: {
+  setWidth: React.Dispatch<React.SetStateAction<number>>;
+  width: number;
+  activeTab: string;
+  setActiveTab: React.Dispatch<React.SetStateAction<string>>;
 }) => {
-  const [showWebList, SetShowWebList] = useState(true);
-  const [showProjectsList, SetShowProjectsList] = useState(true);
-
   const startResizing = (mouseDownEvent: React.MouseEvent) => {
     const handleMouseMove = (mouseMoveEvent: MouseEvent) => {
       const delta = mouseMoveEvent.clientX - mouseDownEvent.clientX;
@@ -53,58 +47,106 @@ const SideBar: React.FC<SideBarProps> = ({
     <div className="flex h-full">
       <div className="text-[#a2aabc] text-lg mt-5 flex w-full">
         <div>
-          <div
-            className="flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] font-bold"
-            onClick={() => SetShowProjectsList(!showProjectsList)}
-          >
-            {showProjectsList ? (
-              <ChevronDownIcon className="w-7 mr-1" />
-            ) : (
-              <ChevronRightIcon className="w-7 mr-1" />
-            )}
-            <p>Projects</p>
-          </div>
-          {showProjectsList ? (
-            <>
+          <div className="sideBarTitle">Manan Sharma</div>
+          <div className="flex flex-col">
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                setActiveTab("home");
+              }}
+            >
               <div
-                className="flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] font-bold"
-                onClick={() => SetShowWebList(!showWebList)}
+                className={`ml-4 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] ${
+                  activeTab === "home" ? "bg-[#1e1e1e] text-yellow_vs" : ""
+                }`}
               >
-                {showWebList ? (
-                  <ChevronDownIcon className="w-7 mr-1  ml-5" />
-                ) : (
-                  <ChevronRightIcon className="w-7 mr-1  ml-5" />
-                )}
-
-                <p>Web</p>
+                <img src={JSIcon} alt="JS Icon" className="w-7 mr-1 ml-2" />
+                <p>Home.ts</p>
               </div>
-              {showWebList ? <WebList /> : null}
-            </>
-          ) : null}
+            </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                setActiveTab("about");
+              }}
+            >
+              <div
+                className={`ml-4 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] ${
+                  activeTab === "about" ? "bg-[#1e1e1e] text-yellow_vs" : ""
+                }`}
+              >
+                <img src={TSIcon} alt="TS Icon" className="w-7 mr-1 ml-2" />
+                <p>About.ts</p>
+              </div>
+            </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                setActiveTab("resume");
+              }}
+            >
+              <div
+                className={`ml-4 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] ${
+                  activeTab === "resume" ? "bg-[#1e1e1e] text-yellow_vs" : ""
+                }`}
+              >
+                <img src={TSIcon} alt="TS Icon" className="w-7 mr-1 ml-2" />
+                <p>Resume.ts</p>
+              </div>
+            </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                setActiveTab("projects");
+              }}
+            >
+              <div
+                className={`ml-4 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] ${
+                  activeTab === "projects" ? "bg-[#1e1e1e] text-yellow_vs" : ""
+                }`}
+              >
+                <img src={TSIcon} alt="TS Icon" className="w-7 mr-1 ml-2" />
+                <p>Projects.ts</p>
+              </div>
+            </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                setActiveTab("contact");
+              }}
+            >
+              <div
+                className={`ml-4 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] ${
+                  activeTab === "contact" ? "bg-[#1e1e1e] text-yellow_vs" : ""
+                }`}
+              >
+                <img src={TSIcon} alt="TS Icon" className="w-7 mr-1 ml-2" />
+                <p>Contact.ts</p>
+              </div>
+            </a>
+          </div>
           <div className="absolute w-full bottom-10 px-6">
             <div className="flex justify-between">
-              <a href="/">
+              <a href="https://github.com/mansha71" target="blank">
                 <img
                   src={GitLogo}
                   alt="Git Logo"
                   className="h-10 w-10 text-yellow_vs hover:cursor-pointer duration-500 hover:scale-125"
                 />
               </a>
-              <a href="/">
-                <img
-                  src={KaggleLogo}
-                  alt="Kaggle Logo"
-                  className="h-10 w-10 text-yellow_vs hover:cursor-pointer duration-500 hover:scale-125"
-                />
-              </a>
-              <a href="/">
+              <a href="https://www.linkedin.com/in/manshr/" target="blank">
                 <img
                   src={LinkedinLogo}
                   alt="Linkedin Logo"
                   className="h-10 w-10 text-yellow_vs hover:cursor-pointer duration-500 hover:scale-125"
                 />
               </a>
-              <a href="/">
+              <a href="mailto:mansharm71@gmail.com" target="blank">
                 <img
                   src={MailLogo}
                   alt="Mail Logo"
@@ -116,56 +158,11 @@ const SideBar: React.FC<SideBarProps> = ({
         </div>
       </div>
       <div
-        className="bg-[#262526] h-full border-r border-gray-700 border-opacity-50 hover:border-opacity-100 hover:border-blue-500 w-3 hover:cursor-col-resize"
         onMouseDown={startResizing}
-      ></div>
+        className="w-1 bg-gray-500 cursor-col-resize"
+      />
     </div>
   );
 };
 
 export default SideBar;
-
-const WebList = () => (
-  <div className="flex flex-col">
-    <a href="/">
-      <div className="ml-12 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a]">
-        <img
-          src={JSIcon}
-          alt="JS Icon"
-          className="w-7 mr-1  ml-5 text-yellow_vs"
-        />
-        <p>First Project</p>
-      </div>
-    </a>
-    <a href="/">
-      <div className="ml-12 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a]">
-        <img
-          src={TSIcon}
-          alt="TS Icon"
-          className="w-7 mr-1  ml-5 text-yellow_vs"
-        />
-        <p>Second Project</p>
-      </div>
-    </a>
-    <a href="/">
-      <div className="flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] focus:bg-slate-300 ml-12">
-        <img
-          src={JSIcon}
-          alt="JS Icon"
-          className="w-7 mr-1  ml-5 text-yellow_vs"
-        />
-        <p>Third Project</p>
-      </div>
-    </a>
-    <a href="/">
-      <div className="flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] ml-12">
-        <img
-          src={JSIcon}
-          alt="JS Icon"
-          className="w-7 mr-1  ml-5 text-yellow_vs"
-        />
-        <p>Fourth Project</p>
-      </div>
-    </a>
-  </div>
-);
